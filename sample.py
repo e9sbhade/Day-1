6d36d61f6543ace88a0d35cc36a850ed59d7694a
e9sbhade <153479270+e9sbhade@users.noreply.github.com>
2023-12-11 11:21:54 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,9 @@
+#Write a function to find the sum of all the numbers from 4212 to 18912512
+
+def sum():
+    addition = 0
+    for i in range(4212,18912513):
+        addition = i + addition
+    print(addition)    
+
+sum()
