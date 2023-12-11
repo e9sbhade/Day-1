2db52202e1f145cd607d0ffc66deb1274d974a60
e9sbhalde <153479270+e9sbhalde@users.noreply.github.com>
2023-12-11 17:55:01 +0530

    Update solver.py

--- solver.py
+++ solver.py
@@ -1,9 +1,3 @@
-#Write a function in python that returns the sum of all the multiples of a list of factors where the multiples are between the start and end, inclusive.
-"""Edit the file solver.py to update the function solver which returns the answer when called as in the below example.
-
-solver([3, 5, 12], 400, 1842)
-"""
-
 def solver(factors,start,end):
  
  numlist =  [] 
